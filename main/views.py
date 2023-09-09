8337b03906cac27c6fb71ee1c623baa444c6fbb8
Alvin Zhafif Afilla <villalvin02@gmail.com>
2023-09-09 23:19:05 +0700

    updating...

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+def show_main(request):
+    context = {
+        'name': 'Alvin Zhafif',
+        'class': 'PBP KKI'
+    }
+
+    return render(request, 'main.html', context)
+
 # Create your views here.
